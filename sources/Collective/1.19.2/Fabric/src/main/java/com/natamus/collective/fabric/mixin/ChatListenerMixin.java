e18377b8d9697185b7797e94cc14f02374192e39
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-11-25 11:44:19 +0100

    compatibility with Chat Heads

--- sources/Collective/1.19.2/Fabric/src/main/java/com/natamus/collective/fabric/mixin/ChatListenerMixin.java
+++ sources/Collective/1.19.2/Fabric/src/main/java/com/natamus/collective/fabric/mixin/ChatListenerMixin.java
@@ -17,41 +17,36 @@
 package com.natamus.collective.fabric.mixin;
 
 import com.natamus.collective.fabric.callbacks.CollectiveChatEvents;
-import net.minecraft.Util;
-import net.minecraft.client.GuiMessageTag;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.client.multiplayer.chat.ChatListener;
-import net.minecraft.client.multiplayer.chat.ChatTrustLevel;
 import net.minecraft.network.chat.*;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import javax.annotation.Nullable;
 import java.time.Instant;
 
 @Mixin(value = ChatListener.class, priority = 1001)
 public abstract class ChatListenerMixin {
-    @Shadow private @Final Minecraft minecraft;
-    @Shadow private long previousMessageTime;
-    @Shadow protected abstract void narrateChatMessage(ChatType.Bound bound, Component component);
-    @Shadow protected abstract void logPlayerMessage(PlayerChatMessage playerChatMessage, ChatType.Bound bound, @Nullable PlayerInfo playerInfo, ChatTrustLevel chatTrustLevel);
+    @Unique ChatType.Bound collective$bound;
+    @Unique PlayerInfo collective$playerInfo;
 
-    @Inject(method = "showMessageToPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;getChat()Lnet/minecraft/client/gui/components/ChatComponent;"), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void showMessageToPlayer(ChatType.Bound bound, PlayerChatMessage playerChatMessage, Component component, PlayerInfo playerInfo, boolean bl, Instant instant, CallbackInfoReturnable<Boolean> cir, ChatTrustLevel chatTrustLevel, GuiMessageTag guiMessageTag, MessageSignature messageSignature, FilterMask filterMask) {
-        //Component newMessage = CollectiveChatEvents.CLIENT_CHAT_RECEIVED.invoker().onClientChat(bound.chatType(), component, playerInfo.getProfile().getId());
-        Component newMessage = CollectiveChatEvents.CLIENT_CHAT_RECEIVED.invoker().onClientChat(bound.chatType(), component, (playerInfo != null ? playerInfo.getProfile().getId() : null));
-		if (component != newMessage) {
-            this.minecraft.gui.getChat().addMessage(newMessage, messageSignature, guiMessageTag);
-            this.narrateChatMessage(bound, playerChatMessage.serverContent());
-            this.logPlayerMessage(playerChatMessage, bound, playerInfo, chatTrustLevel);
-            this.previousMessageTime = Util.getMillis();
-            cir.setReturnValue(true);
-		}
+    @Inject(method = "showMessageToPlayer", at = @At("HEAD"))
+    public void captureParams(ChatType.Bound bound, PlayerChatMessage playerChatMessage, Component component, PlayerInfo playerInfo, boolean bl, Instant instant, CallbackInfoReturnable<Boolean> cir) {
+        collective$bound = bound;
+        collective$playerInfo = playerInfo;
+    }
+
+    @ModifyVariable(method = "showMessageToPlayer", at = @At("HEAD"), argsOnly = true)
+    public Component modifyMessage(Component component) {
+        try {
+            return CollectiveChatEvents.CLIENT_CHAT_RECEIVED.invoker().onClientChat(collective$bound.chatType(), component, (collective$playerInfo != null ? collective$playerInfo.getProfile().getId() : null));
+        } finally {
+            collective$bound = null;
+            collective$playerInfo = null;
+        }
     }
 }
